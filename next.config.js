1bbdb4e8c23d9bf4a5fde61f0ed3b2b0a4b89048
Umoren <samuelumoren365@gmail.com>
2022-11-21 14:24:00 +0100

    added storage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['mynextapponamplify1140557-dev.s3.amazonaws.com'],
+
+
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,70 @@
-export default function Home({ formattedDate }) {
-  return (
-    <>
-      <h1>Static page</h1>
-      <p>This page is static. It was built on {formattedDate}.</p>
-      <p>
-      </p>
-    </>
-  );
+import styles from '../styles/Home.module.css'
+import { Storage } from 'aws-amplify';
+import { useState } from 'react'
+import Image from 'next/image';
+
+const myLoader = ({ src, width, quality }) => {
+  // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+  return `mynextapponamplify1140557-dev.s3.amazonaws.com/${src}?w=${width}&q=${quality || 75}`
 }
 
-export async function getStaticProps() {
-  const buildDate = Date.now();
-  const formattedDate = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "long",
-    timeStyle: "long",
-  }).format(buildDate);
+export default function Home() {
+  const [images, getImages] = useState([])
+  async function onChange(e) {
+    const file = e.target.files[0];
+    try {
+      await Storage.put(file.name, file, {
+        contentType: "image/*", // contentType is optional
+      });
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
 
-  return { props: { formattedDate } };
+  const getUploadedImage = async () => {
+    await Storage.list('') // for listing ALL files without prefix, pass '' instead
+      .then((response) => {
+        getImages(response.results)
+        console.log(response.results[0].key)
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <div>
+          <h1> Nextjs Image Optimization Demo with Amplify Storage</h1>
+
+          <p> This page demonstrates the usage of the next/image component with Amplify Storage as image source.</p>
+        </div>
+
+        <div>
+          <h3> First upload an Image to Amplify Storage</h3>
+          <input type="file" onChange={onChange} />
+
+          <button onClick={getUploadedImage}> get image </button>
+        </div>
+
+        <div>
+          The Uploaded Image:
+          {images.map((image, i) => (
+            <div key={i}>
+
+              <Image
+                loader={myLoader}
+                src={image}
+                alt="Picture of the author"
+                width={500}
+                height={500}
+                unoptimized={true}
+              />
+            </div>
+          ))}
+        </div>
+      </main>
+
+    </div>
+  )
 }
