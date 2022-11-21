3986891ba4ccacff941b13474f3c83d3fbb1c4a5
Umoren <samuelumoren365@gmail.com>
2022-11-21 14:50:11 +0100

    made some changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "mynextapponamplifyb3fa376b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "nextonamplify1": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
